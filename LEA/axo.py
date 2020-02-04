c1d050b972694a8b73f04ad31344296a54b3e6b6
Manuel Sanchez del Rio <srio@esrf.eu>
2020-02-04 14:59:26 -0800

    cleaning

--- LEA/axo.py
+++ LEA/axo.py
@@ -119,37 +119,37 @@ def orthonormalize_a( a, mask=None):
 # function linear_2dgsfit1, u, b, Nz=Nz, mask=mask
 def linear_2dgsfit1(u, b, Nz=None, mask=None):
     """
-     (c) Kenneth A. Goldberg  http://goldberg.lbl.gov  KAGoldberg(at)lbl.gov
-     Lawrence Berkeley National Laboratory
-     09/05/2011, based on linear_gsfit1.pro
-     IDL files: linear_2dgsfit1.pro
-
-      u are the values we want to fit (the target array)
-        on the same domain as b[i].a
-
-     b = linear_2dpolynomials1(N, Ny=Ny, Nz=12, /double)  ;--- create the basis. Ny is the optional domain width
-     v = linear_2dgsfit1(wavefront, b, Nz=8)              ;--- fit a wavefront to 8+1 polynomials (order 8)
-     wf_fit = linear_basis1(v, b)                         ;--- show the fit wavefront phase
-
-     09/05/2011 created this to handle rectangular apertures, but general enough that
-     it should work with any orthogonal polynomials.
-
-     u is an array that you want to fit to the basis, b
-     b is the structure containing the basis polynomials.
-     NZ can be inferred from the size of b, or specified by the user. It's the # of fitting terms.
-     mask is a point-by-point weighting with the same size as u
-
-     IMPORTANT: Never switch from using a mask to not using a mask with the same basis functions.
-        Orthogonalization is performed USING the mask if it's present, or not. So be consistent.
-     IMPORTANT: input array u, b[i].a basis functions, and mask must all be of the same array size
-
-     Note, the scalar values b[i].total_squared are calculated in the orthonormalization.
-       When the polynomials are orthonormal, total_squared is 1.0 by definition.
-       When they are merely orthogonal, they take a positive-definite value.
-       They are calculated with  b[i].total_squared = total( mask * b[i].a^2, /double ), or
-         b[i].total_squared = total( b[i].a^2, /double )  depending on whether or not a mask (weighting)
-         is being used.
-
+    # ; (c) Kenneth A. Goldberg  http://goldberg.lbl.gov  KAGoldberg(at)lbl.gov
+    # ; Lawrence Berkeley National Laboratory
+    # ; 09/05/2011, based on linear_gsfit1.pro
+    # ; IDL files: linear_2dgsfit1.pro
+    # ;
+    # ;  u are the values we want to fit (the target array)
+    # ;    on the same domain as b[i].a
+    # ;
+    # ; b = linear_2dpolynomials1(N, Ny=Ny, Nz=12, /double)  ;--- create the basis. Ny is the optional domain width
+    # ; v = linear_2dgsfit1(wavefront, b, Nz=8)              ;--- fit a wavefront to 8+1 polynomials (order 8)
+    # ; wf_fit = linear_basis1(v, b)                         ;--- show the fit wavefront phase
+    # ;
+    # ; 09/05/2011 created this to handle rectangular apertures, but general enough that
+    # ; it should work with any orthogonal polynomials.
+    #
+    # ; u is an array that you want to fit to the basis, b
+    # ; b is the structure containing the basis polynomials.
+    # ; NZ can be inferred from the size of b, or specified by the user. It's the # of fitting terms.
+    # ; mask is a point-by-point weighting with the same size as u
+    # ;
+    # ; IMPORTANT: Never switch from using a mask to not using a mask with the same basis functions.
+    # ;    Orthogonalization is performed USING the mask if it's present, or not. So be consistent.
+    # ; IMPORTANT: input array u, b[i].a basis functions, and mask must all be of the same array size
+    # ;
+    # ; Note, the scalar values b[i].total_squared are calculated in the orthonormalization.
+    # ;   When the polynomials are orthonormal, total_squared is 1.0 by definition.
+    # ;   When they are merely orthogonal, they take a positive-definite value.
+    # ;   They are calculated with  b[i].total_squared = total( mask * b[i].a^2, /double ), or
+    # ;     b[i].total_squared = total( b[i].a^2, /double )  depending on whether or not a mask (weighting)
+    # ;     is being used.
+    # ;
     """
 
     #
@@ -198,22 +198,22 @@ def linear_2dgsfit1(u, b, Nz=None, mask=None):
 #function linear_basis1, v, b
 def linear_basis(v, b):
     """
-     (c) Kenneth A. Goldberg  http://goldberg.lbl.gov  KAGoldberg(at)lbl.gov
-     Lawrence Berkeley National Laboratory
-     8/03/09
-     IDL files: linear_basis1.pro
-
-     When the linear polynomial basis functions, b[i].a, are already calculated,
-     using linear_polynomials1.pro, or a similar routine, we can calculate an output vector
-     from an input set of coefficients, v
-
-     b = linear_polynomials1(N, Nz=12, /double)  ;--- create the basis
-     v = linear_gsfit1(wavefront, b, Nz=8)       ;--- fit a wavefront to 8 polynomials
-     wf_fit = linear_basis1(v, b)                ;--- show the fit wavefront phase
-
-     v is a vector of coefficients
-     b is a structure containing the basis functions
-     This routine does not worry about the size of the arrays
+    #; (c) Kenneth A. Goldberg  http://goldberg.lbl.gov  KAGoldberg(at)lbl.gov
+    #; Lawrence Berkeley National Laboratory
+    #; 8/03/09
+    #; IDL files: linear_basis1.pro
+    #
+    #; When the linear polynomial basis functions, b[i].a, are already calculated,
+    #; using linear_polynomials1.pro, or a similar routine, we can calculate an output vector
+    #; from an input set of coefficients, v
+    #;
+    #; b = linear_polynomials1(N, Nz=12, /double)  ;--- create the basis
+    #; v = linear_gsfit1(wavefront, b, Nz=8)       ;--- fit a wavefront to 8 polynomials
+    #; wf_fit = linear_basis1(v, b)                ;--- show the fit wavefront phase
+    #;
+    #; v is a vector of coefficients
+    #; b is a structure containing the basis functions
+    #; This routine does not worry about the size of the arrays
 
     :param v:
     :param b:
